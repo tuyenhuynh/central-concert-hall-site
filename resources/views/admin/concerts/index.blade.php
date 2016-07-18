d8fd4d72f7796e8c2259e5d04aca379aa62cc1de
=tuyenhm <tuyenhm93@gmail.com>
2016-07-19 01:04:24 +0300

    Added buttons for CRUD operations

--- resources/views/admin/concerts/index.blade.php
+++ resources/views/admin/concerts/index.blade.php
@@ -16,13 +16,15 @@
                 <th>Фото</th>
                 <th>Время создания</th>
                 <th>Время обновления</th>
+                <th></th>
+                <th></th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
             @if($concerts)
                 @foreach($concerts as $concert)
                     <tr>
-
                         <td>{{$concert->id}}</td>
                         <td><a href ={{"/admin/concerts/".$concert->id."/edit"}}>{{$concert->name}}</a></td>
                         <td>{{ DateTime::createFromFormat('Y-m-d H:i:s', $concert->date_time)->format('d.m.Y')}}</td>
@@ -33,6 +35,15 @@
                         <td>{{$concert->photo_id}}</td>
                         <td>{{$concert->created_at->diffForHumans()}}</td>
                         <td>{{$concert->updated_at->diffForHumans()}}</td>
+                        <td>
+                            <a href ={{"/admin/concerts/".$concert->id}}><span class="fa fa-eye"></span></a>
+                        </td>
+                        <td>
+                            <a href ={{"/admin/concerts/".$concert->id."/edit"}}><span class="fa fa-edit"></span></a>
+                        </td>
+                        <td>
+                            <a href={{"/admin/concerts/".$concert->id."/delete"}}><span class="fa fa-power-off"></span></a>
+                        </td>
                     </tr>
                 @endforeach
             @endif
