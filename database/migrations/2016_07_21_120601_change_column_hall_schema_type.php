7df3af4af2266adc4000511c7579732bb9e14617
=tuyenhm <tuyenhm93@gmail.com>
2016-07-21 16:35:57 +0300

    Change column type of column hall_schema to varchar

---
+++ database/migrations/2016_07_21_120601_change_column_hall_schema_type.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnHallSchemaType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('information', function (Blueprint $table) {
+            $table->string('hall_schema')->change(); 
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('information', function (Blueprint $table) {
+            //
+        });
+    }
+}
