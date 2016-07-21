60c8595921e52354d3cb2730d7ce42fd3526ff1a
=tuyenhm <tuyenhm93@gmail.com>
2016-07-21 16:54:46 +0300

    Remove unused links from admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,16 +41,11 @@
                     </button>
                     <a class="navbar-brand" href="/admin/index">Админская Панель</a>
                 </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Профиль</a></li>
-                        <li><a href="#">Справка</a></li>
-                        <li><a href="/logout">Выход</a></li>
-                    </ul>
-                    <form class="navbar-form navbar-right">
-                        <input type="text" class="form-control" placeholder="Search...">
-                    </form>
-                </div>
+                {{--<div id="navbar" class="navbar-collapse collapse">--}}
+                    {{--<ul class="nav navbar-nav navbar-right">--}}
+                        {{--<li><a href="/logout">Выход</a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
             </div>
         </nav>
 
